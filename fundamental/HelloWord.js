7f163fc02f6064e7adf274df4a8adeffece0cce6
draaaaaaaaaaaaaaa <moh.daffa.ucui@gmail.com>
2022-09-09 21:14:28 +0700

    comit

---
+++ fundamental/HelloWord.js
@@ -0,0 +1,19 @@
+//untuk menampilkan tulisan ke consol atau terminal
+console.log("Hello World!");
+
+//Expression 
+/**
+ * Expression adalah unit kode yang dapat dievaluasi menjadi suatu nilai. 
+ */
+
+ 5; // Baris kode ini merupakan expression karena interpreter akan membaca kode ini dan menghasilkan nilai 5.
+ 2 + 3; // Baris kode ini juga merupakan expression. Interpreter mengevaluasi kode dan juga akan menghasilkan nilai 5.
+
+ //statment
+ /**
+  * tatement adalah instruksi untuk melakukan sebuah aksi tertentu.
+  */
+
+  var yourName;
+  let yourAge;
+  const numberOfDays;  // declaration statement
---
+++ fundamental/Variabel.js
@@ -0,0 +1,33 @@
+let lastname;
+let firsname;
+firsname ="Daffa";
+lastname = "Ramadhan";
+
+console.log(lastname + firsname);
+
+//atau
+
+let nama = "Abdi";
+console.log(nama)
+
+//salah
+/**
+ * let fullName = let lastName; // Error karena let lastName adalah sebuah statement untuk deklarasi variabel. Statement tidak bisa berada di posisi expression.
+let fullName = (lastName = "Skywalker"); // (lastName = "Skywalker") merupakan expression, sehingga kode ini tidak error.
+let fullName = "Luke" + "Skywalker"; // "Luke" + "Skywalker" juga merupakan expression, sehingga kode ini tidak error.
+ */
+
+//aturan
+
+// Harus dimulai dengan huruf atau underscore (_).
+// Dapat terdiri dari huruf, angka, dan underscore (_) dengan berbagai kombinasi.
+// Tidak boleh mengandung spasi (whitespace). Jika penamaan variabel lebih dari dua kata, tuliskan secara camelCase. Contoh firstName, lastName, catName, dll.
+// Tidak boleh mengandung karakter spesial (! . , / \ + * = dll.)
+
+
+// Jika menginisialisasi kembali nilai variabel yang menggunakan const, kita akan mendapati eror “TypeError: Assignment to constant variable.”
+
+const z = 100;
+console.log(z);
+z = 200;
+console.log(z)
